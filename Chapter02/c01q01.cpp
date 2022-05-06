071d366e939a01d4aeefdf28a02a5cbecbab7ac4
P. Williams <mrpwilliams80@gmail.com>
2022-05-06 12:25:56 +0100

    DOCS(c01q01.cpp): v0.1.0
    file header & boilerplate

---
+++ Chapter02/c01q01.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file    c01q01.cpp
+ * @author  P. Williams <mrpwilliams80@gmail.com>
+ * @brief   Write a single-file program (named main.cpp) that reads two separate 
+ *          integers from the user, adds them together, and then outputs the 
+ *          answer.
+ *          The program should use three functions:
+ *          - A function named “readNumber” should be used to get (and return) a 
+ *          single integer from the user.
+ *          - A function named “writeAnswer” should be used to output the 
+ *          answer. This function should take a single parameter and have no 
+ *          return value.
+ *          - A main() function should be used to glue the above functions 
+ *          together.
+ * @version 0.1.0
+ * @date    2022-05-06
+ */
+
+int main()
+{
+    // code here
+
+    return 0;
+}
