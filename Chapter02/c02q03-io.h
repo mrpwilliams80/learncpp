e6f7c9ba798821871a4c335f11ff0453d16dad57
P. Williams <mrpwilliams80@gmail.com>
2022-05-06 13:38:56 +0100

    FIX(c02q03-io.h): solve compilation error
    define cannot use hyphens, removed & compiled

--- Chapter02/c02q03-io.h
+++ Chapter02/c02q03-io.h
@@ -1,8 +1,10 @@
-#ifndef __C02Q03-IO_H__
-#define __C02Q03-IO_H__
+//note: cannot use hyphens in definitions, should probably avoid their useage in
+//      file names.
+#ifndef __C02Q03IO_H__
+#define __C02Q03IO_H__
 
 // function prototypes
 int readNumber ();
 void writeAnswer(int ans);
 
-#endif // __C02Q03-IO_H__
+#endif // __C02Q03IO_H__
