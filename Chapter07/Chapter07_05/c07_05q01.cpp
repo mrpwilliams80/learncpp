0aa96bdf82c5f7eadd75d63df819ba4936b65c89
P. Williams <mrpwilliams80@gmail.com>
2022-05-13 10:43:57 +0100

    DOCS: c07_05q01.cpp v0.1.1
    add getInt() description;

--- Chapter07/Chapter07_05/c07_05q01.cpp
+++ Chapter07/Chapter07_05/c07_05q01.cpp
@@ -8,12 +8,17 @@
  *          result. If an invalid operator is passed into the function, the 
  *          function should print an error. For the division operator, do an 
  *          integer division.
- * @version 0.1.0
+ * @version 0.1.1
  * @date    2022-05-13
  */
 
 #include <iostream>
 
+/**
+ * @brief   Get an int value from the user
+ * 
+ * @return int the value entered by the user
+ */
 int getInt()
 {
     std::cout << "Enter an integer: ";
