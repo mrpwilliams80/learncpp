7947aac6ff0abd09b16e21a1d4e49bafb547d552
P. Williams <mrpwilliams80@gmail.com>
2022-05-15 23:12:26 +0100

    DOCS: c07_07q01.cpp v0.0.0
    boilerplate;

---
+++ Chapter07/Chapter07_09/c07_09q01.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file    c07_09q01.cpp
+ * @author  P. Williams <mrpwilliams80@gmail.com>
+ * @brief   Write a for loop that prints every even number from 0 to 20.
+ * @version 0.0.0
+ * @date    2022-05-15
+ */
+#include <iostream>
+
+int main()
+{
+    // code
+
+    return 0;
+}
