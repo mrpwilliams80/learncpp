e537837c675d95d0475cd4cfb600ac3b0134ccb8
P. Williams <mrpwilliams80@gmail.com>
2022-05-15 23:42:18 +0100

    FEAT: c07_09q02.cpp v0.1.0
    add sumTo();

--- Chapter07/Chapter07_09/c07_09q02.cpp
+++ Chapter07/Chapter07_09/c07_09q02.cpp
@@ -4,10 +4,20 @@
  * @brief   Write a function named sumTo() that takes an integer parameter named 
  *          value, and returns the sum of all the numbers from 1 to value.
  *          For example, sumTo(5) should return 15, which is 1 + 2 + 3 + 4 + 5.
- * @version 0.0.0
+ * @version 0.1.0
  * @date    2022-05-15
  */
 #include <iostream>
+
+int sumTo(int max)
+{
+    int ans {0};
+    for (int x {1}; x <= max; x++)
+    {
+        ans += x;
+    }
+    return ans;
+}
 
 int main ()
 {
