11ce48b3210bbad4e3f6eccd39feada5fc58432a
P. Williams <mrpwilliams80@gmail.com>
2022-05-15 21:53:54 +0100

    FEAT: c07_07q02.cpp v0.2.0
    add colums to output

--- Chapter07/Chapter07_07/c07_07q02.cpp
+++ Chapter07/Chapter07_07/c07_07q02.cpp
@@ -3,7 +3,7 @@
  * @author  P. Williams <mrpwilliams80@gmail.com>
  * @brief   Write a program that prints out the letters a through z along with 
  *          their ASCII codes
- * @version 0.1.1
+ * @version 0.2.0
  * @date    2022-05-15
  */
 
@@ -12,10 +12,17 @@
 int main ()
 {
   char c {'a'};
+  int colCount {0};
+
   while (c <= 'z')
   {
-    std::cout << c << " : " << static_cast <int>(c) << '\n';
+    std::cout << c << " : " << static_cast <int>(c) << '\t';
     c++;
+    ++colCount;
+    if (colCount % 3 == 0)
+    {
+        std::cout << '\n';
+    }
   }
   
   return 0;
