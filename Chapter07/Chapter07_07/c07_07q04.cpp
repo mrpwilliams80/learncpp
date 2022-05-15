59a45e6c64f30a3580c8a7928349fff24128985b
P. Williams <mrpwilliams80@gmail.com>
2022-05-15 22:26:51 +0100

    REFACTOR: c07_07q04.cp v0.1.1
    fix lint errors

--- Chapter07/Chapter07_07/c07_07q04.cpp
+++ Chapter07/Chapter07_07/c07_07q04.cpp
@@ -1,32 +1,43 @@
+/**
+ * @file    c07_07q04.cpp
+ * @author  P. Williams <mrpwilliams80@gmail.com>
+ * @brief   Print this:
+ *                  1
+ *                2 1
+ *              3 2 1
+ *            4 3 2 1
+ *          5 4 3 2 1
+ * @version 0.1.1
+ * @date    2022-05-15
+ */
 #include <iostream>
-
-constexpr int MAX_VALUE {5};
-constexpr int MIN_VALUE {1};
 
 int main ()
 {
-  int lineCount {MAX_VALUE};
-  int charValue {MIN_VALUE};
-  
-  while (lineCount >= MIN_VALUE)
-  {
-    // do each line iteration
-    int charcterCount {MAX_VALUE};
-    while (charcterCount >= MIN_VALUE){
-      if (charcterCount > charValue )
-      {
-        std::cout << "  ";
-        --characterCount;
-      }
-      else
-      {
-        std::cout << charcterCount << ' ';
-        --characterCount;
-      }
-      std::cout << '/n';
+    constexpr int MAX_VALUE {5};
+    constexpr int MIN_VALUE {1};
+
+    int lineCount {MAX_VALUE};
+    int charValue {MIN_VALUE};
+
+    while (lineCount >= MIN_VALUE)
+    {
+        int characterCount {MAX_VALUE};
+        while (characterCount >= MIN_VALUE){
+            if (characterCount > charValue )
+            {
+                std::cout << "  ";
+                --characterCount;
+            }
+            else
+            {
+                std::cout << characterCount << ' ';
+                --characterCount;
+            }
+                std::cout << '\n';
+        }
+        --lineCount;
+        ++charValue;
+        }
+        return 0;
     }
-    --lineCount;
-    ++charValue;
-  }
-  return 0
-}
