39927626e6f0844eee7f8fc2b4d3bf9925cc01cd
P. Williams <mrpwilliams80@gmail.com>
2022-05-15 21:45:36 +0100

    FEAT: c07_07q02.cpp v0.1.1
    fix linter errors

--- Chapter07/Chapter07_07/c07_07q02.cpp
+++ Chapter07/Chapter07_07/c07_07q02.cpp
@@ -1,13 +1,20 @@
-/* Write a program that prints out the letters a through z along with their ASCII ccodes */
+/**
+ * @file    c07_07q02.cpp
+ * @author  P. Williams <mrpwilliams80@gmail.com>
+ * @brief   Write a program that prints out the letters a through z along with 
+ *          their ASCII codes
+ * @version 0.1.1
+ * @date    2022-05-15
+ */
 
 #include <iostream>
 
 int main ()
 {
-  char c {a};
+  char c {'a'};
   while (c <= 'z')
   {
-    std::cout << c << " : " << static_cast<int> c << '/n';
+    std::cout << c << " : " << static_cast <int>(c) << '\n';
     c++;
   }
   
