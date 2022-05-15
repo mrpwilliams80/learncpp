7a3e15c2f61957060c89e9ff5f95d3e5a508f064
P. Williams <mrpwilliams80@gmail.com>
2022-05-15 23:47:33 +0100

    FEAT: c07_09q02.cpp v0.2.0
    add main logic;

--- Chapter07/Chapter07_09/c07_09q02.cpp
+++ Chapter07/Chapter07_09/c07_09q02.cpp
@@ -4,7 +4,7 @@
  * @brief   Write a function named sumTo() that takes an integer parameter named 
  *          value, and returns the sum of all the numbers from 1 to value.
  *          For example, sumTo(5) should return 15, which is 1 + 2 + 3 + 4 + 5.
- * @version 0.1.0
+ * @version 0.2.0
  * @date    2022-05-15
  */
 #include <iostream>
@@ -21,7 +21,14 @@ int sumTo(int max)
 
 int main ()
 {
-    //code
+    // prompt
+    std::cout << "Enter an integer: ";
+    int num {};
+    std::cin >> num;
+
+    // output
+    std::cout   << "The triangle number of " << num
+                << " is " << sumTo(num) << '\n';
 
     return 0;
 }
